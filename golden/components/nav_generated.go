788c530e9619eecbdb1d53649915f6693fbfa8e1
Aaron Schlesinger <aaschles@microsoft.com>
2018-07-03 12:38:20 -0700

    making the nav include a link to the blog

--- golden/components/nav_generated.go
+++ golden/components/nav_generated.go
@@ -9,56 +9,55 @@ import (
 
 
 var NavTemplate =  `<nav>
-	<ul>
-		<li>This is an item</li>
-		<li>This is another item</li> 
-	</ul>
+    <ul>
+        <!-- Routes is builtin and the router holds all the routes in there -->
+        <li><a href="{{ .Routes.Blog }}">Blog</a></li>
+    </ul>
 </nav>` 
 var NavStyles = `
-	nav {
-		border-bottom: 1px solid rgba(170, 30, 30, 0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	.selected {
-		position: relative;
-		display: inline-block;
-	}
-
-	.selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(170, 30, 30);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+    nav {
+        border-bottom: 1px solid rgba(170, 30, 30, 0.1);
+        font-weight: 300;
+        padding: 0 1em;
+    }
+    
+    ul {
+        margin: 0;
+        padding: 0;
+    }
+    /* clearfix */
+    
+    ul::after {
+        content: '';
+        display: block;
+        clear: both;
+    }
+    
+    li {
+        display: block;
+        float: left;
+    }
+    
+    .selected {
+        position: relative;
+        display: inline-block;
+    }
+    
+    .selected::after {
+        position: absolute;
+        content: '';
+        width: calc(100% - 1em);
+        height: 2px;
+        background-color: rgb(170, 30, 30);
+        display: block;
+        bottom: -1px;
+    }
+    
+    a {
+        text-decoration: none;
+        padding: 1em 0.5em;
+        display: block;
+    }
 `
 
 
