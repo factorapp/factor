27e5ede5b8cc5f74469b9fbc831ee68d694dda53
Aaron Schlesinger <aaschles@microsoft.com>
2018-07-03 12:25:31 -0700

    making a todo client

--- golden/models/init.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// factor.RPC is a generated typesafe thing that wraps net/rpc
-var client factor.RPCClient
-
-func init() {
-	client = factor.NewRPCClient()
-}
--- golden/models/todo.go
+++ golden/models/todo.go
@@ -4,6 +4,7 @@ import (
 	"github.com/satori/go.uuid"
 )
 
+// Todo is a model
 type Todo struct {
 	ID          uuid.UUID
 	Name        string
@@ -11,26 +12,11 @@ type Todo struct {
 }
 
 // AddTodo is the server-side implementation to add a Todo to the
-// database
-//
-// NewTodo is a generated Todo. It is the same as Todo but also have a
-// context.Context in it. Maybe this?
-//
-//	type NewTodo struct {
-//		Ctx context.Context
-//		ID uuid.UUID
-//		Name string
-//		Description  string
-//	}
-//
-// TodoResp has some kind of error or status thing in it. Maybe this?
-//
-//	type TodoResp struct {
-//		Result error
-//	}
+// database.
 //
 // note: I feel like this should be in a separate package that holds
 // all the server stuff. I'll leave it here for now
 func AddTodo(arg NewTodo, retVal TodoResp) error {
+	// database is something that hooks up to a persistent database
 	return database.AddTodo(arg.Ctx)
 }
