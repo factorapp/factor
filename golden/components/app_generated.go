b4733309cfe559b092fdb65f70b49117092910ab
Aaron Schlesinger <aaschles@microsoft.com>
2018-07-03 14:29:39 -0700

    rendering a dynamic app template

--- golden/components/app_generated.go
+++ golden/components/app_generated.go
@@ -1,31 +1,38 @@
-
 package components
 
 import (
-	
+	"bytes"
+	"html/template"
+	"log"
+
 	"github.com/bketelsen/factor/markup"
 )
 
-
-
-var AppTemplate =  `<main>
-<Nav />
-	<div>Brian Was Here</div>
-</main>` 
+var AppTemplate = `<main>
+    <div>{{.Greeting}}</div>
+	<a onclick="OnClick" href="/blog">Blog</a>
+</main>`
 var AppStyles = `
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+    main {
+        position: relative;
+        max-width: 56em;
+        background-color: white;
+        padding: 2em;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
 `
 
-
 func (t *App) Render() string {
-	return AppTemplate
+	b := new(bytes.Buffer)
+	tpl, err := template.New("app").Parse(AppTemplate)
+	if err != nil || tpl == nil {
+		log.Printf("couldn't parse app template")
+		return ""
+	}
+	log.Printf("parsed app template, continuing")
+	tpl.Execute(b, t)
+	return b.String()
 }
 
 func init() {
