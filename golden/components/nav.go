1363a29f204e7b89feb3ff5baf78434225d647a0
Aaron Schlesinger <aaschles@microsoft.com>
2018-07-03 11:26:41 -0700

    adding required import

--- golden/components/nav.go
+++ golden/components/nav.go
@@ -1,10 +1,14 @@
 package components
 
+import (
+	"github.com/bketelsen/factor/markup"
+)
+
 type Nav struct {
 	MyProp      string
 	CurrentPath string
 }
 
 func (n *Nav) OnAfterPrint(e *markup.Event) {
-	
+
 }
