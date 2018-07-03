4265452d5ac6bb99a70975b221e57d713823d038
Aaron Schlesinger <aaschles@microsoft.com>
2018-07-03 12:44:57 -0700

    bringing the wasm server into golden

--- golden/main.go
+++ golden/main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/bketelsen/factor/golden/components"
 	"github.com/bketelsen/factor/markup"
 )
 
-func main() {
+func wasmHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/wasm")
+	http.ServeFile(w, r, "example.wasm")
+}
 
+func main() {
 	c := &components.App{}
 	_, err := markup.MountBody(c)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/example.wasm", wasmHandler)
+	log.Fatal(http.ListenAndServe(":3000", mux))
+
 }
