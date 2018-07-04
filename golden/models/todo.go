e9109b3d327cb4a31287bff060beb61b453260dc
Aaron Schlesinger <aaschles@microsoft.com>
2018-07-03 20:04:23 -0700

    Adding comments to generated code

--- golden/models/todo.go
+++ golden/models/todo.go
@@ -10,13 +10,3 @@ type Todo struct {
 	Name        string
 	Description string
 }
-
-// AddTodo is the server-side implementation to add a Todo to the
-// database.
-//
-// note: I feel like this should be in a separate package that holds
-// all the server stuff. I'll leave it here for now
-func AddTodo(arg NewTodo, retVal TodoResp) error {
-	// database is something that hooks up to a persistent database
-	return database.AddTodo(arg.Ctx)
-}
